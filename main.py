855843374e0d7b9e16d4c16b1f9f90983f9729ac
Sanika Shahapurkar <sanika.shahapurkar@gmail.com>
2023-02-23 15:52:08 -0800

    pls

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from __init__ import app,db  # Definitions initialization
 from model.jokes import initJokes
 from model.users import initUsers
 from model.players import initPlayers
+from model.clients import initClients
 
 
 # setup APIs
@@ -15,6 +16,7 @@ from api.covid import covid_api # Blueprint import api definition
 from api.joke import joke_api # Blueprint import api definition
 from api.user import user_api # Blueprint import api definition
 from api.player import player_api
+from api.client import client_api 
 
 
 # setup App pages
@@ -26,6 +28,7 @@ app.register_blueprint(covid_api) # register api routes
 app.register_blueprint(user_api) # register api routes
 app.register_blueprint(player_api)
 app.register_blueprint(app_projects) # register app pages
+app.register_blueprint(client_api)
 
 @app.errorhandler(404)  # catch for URL not found
 def page_not_found(e):
@@ -46,6 +49,7 @@ def activate_job():  # activate these items
     initJokes()
     initUsers()
     initPlayers()
+    initClients()
 
 # this runs the application on the development server
 if __name__ == "__main__":
