fe27b27e9c6188a65c04a29661086074a15ab100
e-shen2022 <aimashen.2017@gmail.com>
2023-02-23 18:30:59 -0800

    main.py

--- main.py
+++ main.py
@@ -6,16 +6,16 @@ from flask import render_template  # import render_template from "public" flask 
 # import "packages" from "this" project
 from __init__ import app,db  # Definitions initialization
 from model.jokes import initJokes
-from model.users import initUsers
+#from model.users import initUsers
 from model.players import initPlayers
-
+from model.skintypes import initSkinTypes
 
 # setup APIs
 from api.covid import covid_api # Blueprint import api definition
 from api.joke import joke_api # Blueprint import api definition
-from api.user import user_api # Blueprint import api definition
+#from api.user import user_api # Blueprint import api definition
 from api.player import player_api
-
+from api.skintype import skintype_api # Blueprint import api definition
 
 # setup App pages
 from projects.projects import app_projects # Blueprint directory import projects definition
@@ -23,9 +23,10 @@ from projects.projects import app_projects # Blueprint directory import projects
 # register URIs
 app.register_blueprint(joke_api) # register api routes
 app.register_blueprint(covid_api) # register api routes
-app.register_blueprint(user_api) # register api routes
+#app.register_blueprint(user_api) # register api routes
 app.register_blueprint(player_api)
 app.register_blueprint(app_projects) # register app pages
+app.register_blueprint(skintype_api) # register api routes
 
 @app.errorhandler(404)  # catch for URL not found
 def page_not_found(e):
@@ -44,8 +45,10 @@ def stub():
 def activate_job():  # activate these items 
     db.init_app(app)
     initJokes()
-    initUsers()
+    #initUsers()
     initPlayers()
+    initSkinTypes()
+
 
 # this runs the application on the development server
 if __name__ == "__main__":
